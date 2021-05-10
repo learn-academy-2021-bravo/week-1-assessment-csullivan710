46f79c7060ef61f3826b6079009749d566bbf7f7
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2021-05-10 09:38:08 -0500

    initial commit

--- code-challenges.js
+++ code-challenges.js
@@ -9,9 +9,29 @@
 // --------------------1) Create a function that takes in a number and determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Fahrenheit.
 // Use the test variables provided below. Expected output: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp1 = 35
+/*var temp1 = 35
 var temp2 = 350
 var temp3 = 212
+//create function expression called boiling
+//implement for loop
+//create if function expression for less than < 212
+//return ${} 'is below boiling point'
+//else if = 212 return ${} is at boiling point
+//else return is above boiling point
+const boiling = (temp) => {
+for(let i = 0; i < temp.length; i++){
+     if(temp[i] < 212){
+         return `${temp} is below boiling point`
+     } else if(
+         temp = 212){
+             return `${temp} is at boiling point`
+         }else {
+             return `${temp} is above boiling point`
+         }
+     }
+    }
+    let temp = [35, 212, 310]
+    console.log(boiling(temp))*/
 
 
 
@@ -21,6 +41,18 @@ var temp3 = 212
 // Use the test variable provided below. Expected output: [15, 35, 0, 30, -45]
 
 var myNumbers1 = [3, 7, 0, 6, -9]
+//create a new array and return the array multiplied by 5 
+//implement for loop
+//declare array variable
+//create function expression multiplied by 5
+/*const multByFive = (array) => {
+    let newArray = []
+    for(let i =0; i<myNumbers1.length; i++){
+        newArray.push(array[i] * 5)
+    }
+    return newArray
+}
+console.log(multByFive(myNumbers1))*/
 
 
 
@@ -30,6 +62,16 @@ var myNumbers1 = [3, 7, 0, 6, -9]
 // Use the test variable provided below. Expected output: [-7, 9, 13]
 
 var myNumbers2 = [8, -7, 0, 6, 2, 9, 13]
+/*const oddNum = (myNumbers2) => {
+let newArray = []
+for(let i = 0; i<myNumbers2.length; i++){
+    if(myNumbers2[i] % 2 !==0){
+        newArray.push(myNumbers2[i])
+    }
+}
+return newArray
+}
+console.log(oddNum(myNumbers2))*/
 
 
 
@@ -40,10 +82,25 @@ var myNumbers2 = [8, -7, 0, 6, 2, 9, 13]
 
 var stringWithVowels1 = "HeyThereLearnStudent"
 var stringWithVowels2 = "ILoveJavaScript"
+//Create a function
+//Remove all vowels from a string
+//put it back together
+/*
+const noVowels = (strings) =>{
+    let newArray = ""
+    for(let i = 0; i<stringWithVowels1.length; i++){
+        let lowerCase = strings[i].toLowerCase
+        if(strings[i] !== "a" && lowerCase !== "e" && lowerCase !== i && lowerCase !== "o" && lowerCase !=="u"){
+            newArray+=lowerCase
+        }
+    }
+    return newArray
 
+}
+console.log(noVowels(stringWithVowels1))
+console.log(noVowels(stringWithVowels2))
 
-
-
+*/
 
 // --------------------5) Copy/paste your code from #4. Refactor your code to include non-string edge cases. Inform your user if the variable passed into the vowel removal function is not a string.
 // Use the test variables provided below. Expected output: "true is not a string" "42 is not a string" "mCdngMstr"
@@ -52,3 +109,20 @@ var stringWithVowels2 = "ILoveJavaScript"
 var refactorTester1 = true
 var refactorTester2 = 42
 var refactorTester3 = "IAmACodingMaster"
+
+
+//change return to show type of input
+const noVowels = (strings) =>{
+    let newArray = ""
+    for(let i = 0; i<stringWithVowels1.length; i++){
+        let lowerCase = strings[i].toLowerCase
+        if(strings[i] !== "a" && lowerCase !== "e" && lowerCase !== i && lowerCase !== "o" && lowerCase !=="u"){
+            newArray+=lowerCase
+        }
+    }
+    return newArray.typeOf
+
+
+}
+console.log(noVowels(stringWithVowels1))
+console.log(noVowels(stringWithVowels2))
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,44 +8,45 @@
 // --------------------1) What will this log?
 
 var cohort = "Bravo 2021"
-// console.log(cohort.length)
+ //console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 10
+// b) Verify and explain: 10, because you were asking for the length of the string passed as cohort
 
 
 // --------------------2) What will this log?
 
 var greeting = "Hello World!"
-// console.log(greeting[3])
+ //console.log(greeting[3])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: l
+// b) Verify and explain:It passed l because you were asking for what is at the third index of what was passed, that being what is at the third
+    //index starting at 0 of "Hello World!"
 
 
 // --------------------3) What will this log?
 
 var languages = ["JavaScript", "Ruby", "Python", "C++"]
 var index = 1
-// console.log(languages[index])
+ //console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:Ruby
+// b) Verify and explain: You are asking what is the at index position one of thise array and each string is separate because they are separated by a comma
 
 
 // --------------------4) What will this log?
 
 var weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+ //console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: error
+// b) Verify and explain: weekendDays.toUpperCase is not a function
 
 
 // --------------------5) What will this log?
 
 var dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+ console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: number
+// b) Verify and explain: you did not specify how many times to run the function
